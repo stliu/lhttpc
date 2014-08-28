9ef5035cb94c8f464e40ecc0b1fae1516ed897be
Strong Liu <stliu@hibernate.org>
2014-08-29 01:48:31 +0800

    change to avoid invalid-file-encode error

--- test/lhttpc_tests.erl
+++ test/lhttpc_tests.erl
@@ -24,7 +24,7 @@
 %%% ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %%% ----------------------------------------------------------------------------
 
-%%% @author Oscar Hellström <oscar@hellstrom.st>
+%%% @author Oscar Hellstr <oscar@hellstrom.st>
 -module(lhttpc_tests).
 
 -export([test_no/2]).
--- util/make_doc.erl
+++ util/make_doc.erl
@@ -24,7 +24,7 @@
 %%% ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %%% ----------------------------------------------------------------------------
 
-%%% @author Oscar Hellström <oscar@hellstrom.st>
+%%% @author Oscar Hellstr <oscar@hellstrom.st>
 -module(make_doc).
 -export([edoc/0]).
 
